066a01f0de95dc4cc8ba1219f30701dde91760d7
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-02 07:36:39 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+				hello: ':wave:',
+    },
+
+    discord: {
+        token: 'ODQ0NDgxMzg2Nzc3MjgwNTIz.YKTClA.Npm58usv9cI6lL7e7K7BBafJNzU',
+        prefix: '!n ',
+        activity: '!n help // Version 1.5.0',
+     },
+
+
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
