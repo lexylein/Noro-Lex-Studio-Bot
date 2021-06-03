7af1d8628a054162ac74b8e5b8035055bd3c4f93
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-03 10:25:09 +0200

    Update wartungen.js

--- commands/owner/wartungen.js
+++ commands/owner/wartungen.js
@@ -8,7 +8,7 @@ module.exports = {
     	
     	message.channel.bulkDelete(1);
     	
-    	if(message.author.id === "612589827262644234") return message.channel.send('the bot is in maintenance work');
+    	if(message.author.id === "Owner ID") return message.channel.send('the bot is in maintenance work');
     	
     	message.channel.send('you are not a Owner from the Bot')
     	
