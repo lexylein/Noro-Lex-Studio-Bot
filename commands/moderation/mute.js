25d0765ad59cea38e9c7038f5b0f08435abe0574
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-03 10:52:31 +0200

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'userid',
+    name: 'mute',
     aliases: [],
     category: 'Moderation',
-    utilisation: '{prefix}userid [user]',    	
+    utilisation: '{prefix}mute [user]',    	
 
     execute(client, message) {
     	
