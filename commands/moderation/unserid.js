0183fbd540c9d1da7c1dc90476f07cdfa601cec0
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-03 08:27:29 +0200

    Update unserid.js

--- commands/moderation/unserid.js
+++ commands/moderation/unserid.js
@@ -6,7 +6,7 @@ module.exports = {
 
     execute(client, message) {
     	
-       (!message.member.hasPermission('KICK_MEMBERS'))
+      if (!message.member.hasPermission('KICK_MEMBERS'))
         return message.reply('You do not have permissions to use that command');
         
         message.channel.send(`the id from ${message.mentions.users.first().username} id ${message.mentions.users.first().id}`)
