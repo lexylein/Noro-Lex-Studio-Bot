a29cb1e128d4f51d444024b3dbb347e942f50cab
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-03 10:51:42 +0200

    Update unserid.js

--- commands/moderation/unserid.js
+++ commands/moderation/unserid.js
@@ -6,10 +6,12 @@ module.exports = {
 
     execute(client, message) {
     	
-      if (!message.member.hasPermission('KICK_MEMBERS'))
+    	 if (!message.member.hasPermission('KICK_MEMBERS'))
         return message.reply('You do not have permissions to use that command');
         
+        if (!message.mentions.users.first()) return message.reply("You need to mention someone to see a user id"); 
+        
         message.channel.send(`the id from ${message.mentions.users.first().username} id ${message.mentions.users.first().id}`)
-  }
+   }
   
 };
