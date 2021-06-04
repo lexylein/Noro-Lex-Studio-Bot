e2410f1ccfdd4daf52b81b454bab7d4131e27118
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-04 08:50:09 +0200

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -4,14 +4,48 @@ module.exports = {
     category: 'Moderation',
     utilisation: '{prefix}mute [user]',    	
 
-    execute(client, message) {
+    execute(client, message, args) {
     	
-    	 if (!message.member.hasPermission('MANAGE_ROLE'))
-        return message.reply('You do not have permissions to use that command');
-        
-        if (!message.mentions.users.first()) return message.reply("You need to mention someone to mute a user"); 
-        
-        
-   }
+    	if (!message.member.hasPermission("MANAGE_ROLES")) {
+                    return message.reply(
+                        "You do not have the permission to use that command."
+                    );
+                }
+
+                if (args.length === 0)
+                    return message.reply(" need to mention someone to mute a user");
+
+                let user = message.guild.member(
+                    message.mentions.users.first() ||
+                        message.guild.members.cache.get(args[0])
+                );
+
+                if (!user) return message.reply("Couldn't find user.");
+
+                if (user.hasPermission("MANAGE_MESSAGES"))
+                    return message.reply("Can't mute user!");
+
+                
+
+                
+                        
+                
+
+                let mutetime = args[1];
+                if (!mutetime)
+                    return message.reply("You didn't specify a time!");
+
+('addrole command')
+                return message.reply(`${message.mentions.users.first().username} has been muted for `);
+
+                setTimeout(function () {
+                    user.removeRole(muterole.id);
+                    message.channel.send(`${message.mentions.users.first().username} has been unmuted!`);
+                }, ms(mutetime));
+    
+
+    
   
+
+    }
 };
