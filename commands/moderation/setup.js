45692708ef2cd98f123a7679f5dd605cc806fa8d
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-05 13:45:21 +0200

    Update setup.js

--- commands/moderation/setup.js
+++ commands/moderation/setup.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message) {
        if (!message.member.hasPermission('ADMINISTRATOR'))
         return message.reply('You do not have permissions to use that command');
-				 message.member.send(`Please move the role ** Noro Lex Studio Bot ** in first place in the order of the roles top, create a ** Red ** role, create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
+				 message.member.send(`Please move the role ** Noro Lex Studio Bot ** in first place in the order of the roles top, create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
 				 
 				
 				 
