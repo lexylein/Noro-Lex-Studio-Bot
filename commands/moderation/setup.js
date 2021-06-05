cdee309c3b95ffa2a6d01e8efb84a6f064b251d7
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-05 13:33:57 +0200

    Update setup.js

--- commands/moderation/setup.js
+++ commands/moderation/setup.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message) {
        if (!message.member.hasPermission('ADMINISTRATOR'))
         return message.reply('You do not have permissions to use that command');
-				 message.member.send(`Create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
+				 message.member.send(`create a ** Red ** role, create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
 				 
 				
 				 
