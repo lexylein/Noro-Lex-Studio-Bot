b3d0e2ea8b3bad9d12160a82a46617000c9806aa
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-05 13:13:20 +0200

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -26,12 +26,18 @@ module.exports = {
                     return message.reply("Can't mute user!");
 
                     
-                    let role = message.guild.roles.cache.find(r => r.name === "Muted");
+                    let verifiziert1 = message.guild.roles.cache.find(r => r.name === "Verifiziert");
+                    
+                    let verifiziert2 = message.guild.roles.cache.find(r => r.name === "verifiziert");
+                    
+                    let mute = message.guild.roles.cache.find(r => r.name === "Muted");
                     
                     let member = message.mentions.members.first();
 
 
-                member.roles.add(role).catch(console.error);
+                member.roles.remove(verifiziert1).catch(console.error);
+                member.roles.remove(verifiziert2).catch(console.error);
+                member.roles.add(mute).catch(console.error);
                 return message.reply(`${message.mentions.users.first().username} has been muted!`);
 
     }
