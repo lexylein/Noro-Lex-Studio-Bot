761efb7f7ca75276888d207c9ca86c72a14a6fae
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-05 13:03:06 +0200

    Update and rename musicsetup.js to setup.js

--- commands/moderation/setup.js
+++ commands/moderation/setup.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message) {
        if (!message.member.hasPermission('ADMINISTRATOR'))
         return message.reply('You do not have permissions to use that command');
-				 message.member.send(`Create a ** Music ** role and add the role to you.`); 
+				 message.member.send(`Create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
 				 
 				
 				 
