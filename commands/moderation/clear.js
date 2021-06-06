6213e9d5c2e83ab080cbe5bcfc3d6ee050274a6f
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-06 17:17:49 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -2,17 +2,20 @@ module.exports = {
     name: 'clear',
     aliases: [],
     category: 'Moderation',
-    utilisation: '{prefix}clear',    	
-    	execute(client, message, args) {
-    	if (!message.member.hasPermission('MANAGE_MESSAGES'))
-        return message.reply('You do not have permissions to use that command');
-        message.channel.bulkDelete(30);
-        
-        message.channel.send(`i Deletet 30 messages`)
-                  .then((sent) => {
-          setTimeout(() => {
-            sent.delete();
-          }, 2500);
-        });
+    utilisation: '{prefix}clear [amount]',
+
+        execute(bot, message, args) {
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You Don't Have Sufficient Permissions!- [MANAGE_MESSAGES]")
+        if (isNaN(args[0]))
+            return message.channel.send('**Please Supply A Valid Amount To Delete Messages!**');
+
+        if (args[0] > 100)
+            return message.channel.send("**Please Supply A Number Less Than 100!**");
+
+        if (args[0] < 1)
+            return message.channel.send("**Please Supply A Number More Than 1!**");
+
+        message.channel.bulkDelete(args[0])
+            .then(messages => message.channel.send(`**Succesfully deleted \`${messages.size}/${args[0]}\` messages**`).then(msg => msg.delete({ timeout: 5000 }))).catch(() => null)
     }
 };
