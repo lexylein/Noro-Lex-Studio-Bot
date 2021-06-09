f97967fcad340b36235c71fae651199571eae8cf
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-09 16:11:12 +0200

    Update primehelp.js

--- commands/core/primehelp.js
+++ commands/core/primehelp.js
@@ -7,11 +7,23 @@ module.exports = {
     utilisation: '{prefix}primehelp',
     execute(client, message, args) {
     
-    let primeEmbed = new MessageEmbed()
-        .setColor('#ff0000')
-        .setTitle("PrimeHelp")
-        .addField(`**Prime**`, `\`joke\``)
     
-    message.channel.send(primeEmbed)
+    
+if (!args[0]) {
+            const prime = ('joke');
+
+            message.channel.send({
+                embed: {
+                    color: '#ff0000',
+                    author: { name: 'PrimeHelp pannel' },
+                    footer: { text: 'This bot made by Noro Lex Studio (Noro Lex Studio Bot)' },
+                    fields: [
+                      { name: '**Prime**', value: prime },
+                    ],
+                    timestamp: new Date(),
+                    description: `${client.emotes.success} - Support Server: https://discord.gg/GqT6fUt6z9`,
+                },
+            });
+        }
 }
 };
