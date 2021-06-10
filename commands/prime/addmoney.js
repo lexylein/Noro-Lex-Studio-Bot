adee74ad2c9a39b92109ab2a16095d1a7a1c8458
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-10 09:47:39 +0200

    Create addmoney.js

---
+++ commands/prime/addmoney.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+    name: 'addmoney',
+    aliases: ['am'],
+    category: 'Prime',
+    utilisation: '{prefix}addmoney [user] [amount]',
+     execute(bot, message, args) {
+     	
+     	  if(!message.guild.id === "784348476220768280") return message.channel.send('the server has no prime from the bot');
+    	  
+    	  if(!message.guild.id === "836299465051996212") return message.channel.send('the server has no prime from the bot');
+    	  
+    	  if(!message.guild.id === "770040861261430794") return message.channel.send('the server has no prime from the bot');
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("❌ You Do Not Have Permissions To Add Money! - [ADMINISTRATOR]");
+        if (!args[0]) return message.channel.send("**Please Enter A User!**")
+
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLocaleLowerCase());
+        if (!user) return message.channel.send("**Enter A Valid User!**")
+        if (!args[1]) return message.channel.send("**Please Enter A Amount!**")
+        if (isNaN(args[1])) return message.channel.send(`**❌ Your Amount Is Not A Number!**`);
+        if (args[0] > 10000) return message.channel.send("**Cannot Add That Much Amount!**")
+        db.add(`money_${user.id}`, args[1])
+        let bal = db.fetch(`money_${user.id}`)
+
+        let moneyEmbed = new MessageEmbed()
+            .setColor("GREEN")
+            .setDescription(`✅ Added ${args[1]} coins\n\nNew Balance: ${bal}`);
+        message.channel.send(moneyEmbed)
+
+    }
+};
