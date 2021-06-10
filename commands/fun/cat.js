8f98e8b37ecf439b03d8edd8b87e1a0a71f5aa1b
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-10 14:16:27 +0200

    Update cat.js

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -1,12 +1,11 @@
 const superagent = require("snekfetch");
 const Discord = require('discord.js')
 
-
 module.exports = {
-  name: "cat",
-  category: "fun",
-description: "Sends a random image of a cat",
-usage: "[command]",
+    name: 'cat',
+    aliases: [],
+    category: 'fun',
+    utilisation: '{prefix}cat',
 execute(client, message, args, level) {
 //command
 superagent.get('https://nekos.life/api/v2/img/meow')
