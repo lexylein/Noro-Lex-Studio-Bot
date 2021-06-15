b983f7d50f2997d297dd9f1191534c9b344547e2
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-15 13:47:22 +0200

    Update cat.js

--- commands/prime/cat.js
+++ commands/prime/cat.js
@@ -17,13 +17,13 @@ superagent.get('https://nekos.life/api/v2/img/meow')
   .setFooter(`owo`)
   .setURL(response.body.url);
     
-    if(message.guild.id === "784348476220768280") return message.channel.send(lewdembed);
+    if(message.guild.id === "prime 1 server id") return message.channel.send(lewdembed);
 		
-	if(message.guild.id === "836299465051996212") return message.channel.send(lewdembed);
+	if(message.guild.id === "prime 2 server id") return message.channel.send(lewdembed);
 		
-	if(message.guild.id === "770040861261430794") return message.channel.send(lewdembed);
+	if(message.guild.id === "prime 3 server id") return message.channel.send(lewdembed);
        
-    if(message.guild.id === "769163085113327638") return message.channel.send(lewdembed);
+    if(message.guild.id === "prime 3 server id") return message.channel.send(lewdembed);
 
 
         
