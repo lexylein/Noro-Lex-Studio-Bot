3b25dff0e458515c52b020c09e5b8d1115f46015
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-15 13:50:26 +0200

    Update dog.js

--- commands/prime/dog.js
+++ commands/prime/dog.js
@@ -18,13 +18,13 @@ superagent.get('https://nekos.life/api/v2/img/woof')
   .setURL(response.body.url);
 
     
-     if(message.guild.id === "784348476220768280") return message.channel.send(lewdembed);
+     if(message.guild.id === "prime 1 server id") return message.channel.send(lewdembed);
 		
-	if(message.guild.id === "836299465051996212") return message.channel.send(lewdembed);
+	if(message.guild.id === "prime 2 server id") return message.channel.send(lewdembed);
 		
-	if(message.guild.id === "770040861261430794") return message.channel.send(lewdembed);
+	if(message.guild.id === "prime 3 server id") return message.channel.send(lewdembed);
        
-    if(message.guild.id === "769163085113327638") return message.channel.send(lewdembed);
+    if(message.guild.id === "prime 4 server id") return message.channel.send(lewdembed);
 
 
         
