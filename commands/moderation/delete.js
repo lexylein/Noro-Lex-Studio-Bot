737649d34e9a12ffd7fa44a8d1e5ac1849355f8c
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-17 10:38:32 +0200

    Update delete.js

--- commands/moderation/delete.js
+++ commands/moderation/delete.js
@@ -1,17 +1,32 @@
 module.exports = {
-    name: 'delete',
-    aliases: ['del'],
+    name: 'add',
+    aliases: [],
     category: 'Moderation',
-    utilisation: '{prefix}delete',
-	execute(client, message, args) {
-		   
-		
+    utilisation: '{prefix}add [User]',
+	execute(client, message, args, prefix) {
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.reply(
+                        "You do not have the permission to use that command."
+                    );
+                }
 		if(message.channel.name.includes('ticket-')) {
-			message.channel.delete();
+			const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]);
+			if(!member) {
+				return message.channel.send(`Incorrect Usage! Correct Usage:${prefix}add <member>`);
+			}
+			try{
+				message.channel.updateOverwrite(member.user, {
+					VIEW_CHANNEL: true,
+					SEND_MESSAGES: true,
+					ATTACH_FILES: true,
+					READ_MESSAGE_HISTORY: true,
+				}).then(() => {
+					message.channel.send(`Successfully added ${member} to ${message.channel}`);
+				});
+			}
+			catch(e) {
+				return message.channel.send('An error occurred, please try again!');
+			}
 		}
-		else {
-			return message.reply('you cannot use this command here. Please use this command when you want to delete a ticket.');
-		}
-		
 	},
 };
