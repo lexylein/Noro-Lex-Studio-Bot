957808de39f20349887f8a380f8c4cf376972243
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-17 10:43:33 +0200

    Update new.js

--- commands/moderation/new.js
+++ commands/moderation/new.js
@@ -22,7 +22,7 @@ module.exports = {
 			type: 'text',
 		}).then(async channel => {
 			message.reply(`you have successfully created a ticket! Please click on ${channel} to view your ticket.`);
-			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`${prefix}close\``);
+			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`!n delete\``);
 			let logchannel = message.guild.channels.cache.find(channel => channel.name === `ticket-logs`)
 			if(logchannel) {
 				logchannel.send(`Ticket ${message.author.id} created. Click the following to veiw <#${channel.id}>`);
