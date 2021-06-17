0033e858d18e3981d408833b7694ff2557331b8f
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-17 10:35:07 +0200

    Create delete.js

---
+++ commands/moderation/delete.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'delete',
+    aliases: ['del'],
+    category: 'Moderation',
+    utilisation: '{prefix}delete',
+	execute(client, message, args) {
+		   
+		
+		if(message.channel.name.includes('ticket-')) {
+			message.channel.delete();
+		}
+		else {
+			return message.reply('you cannot use this command here. Please use this command when you want to delete a ticket.');
+		}
+		
+	},
+};
