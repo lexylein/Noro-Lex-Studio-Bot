d98213fc9df9040b353a5debbf7f891935b0d321
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-21 09:20:15 +0200

    Update setup.js

--- commands/moderation/setup.js
+++ commands/moderation/setup.js
@@ -9,8 +9,20 @@ module.exports = {
         return message.reply('You do not have permissions to use that command');
 				 message.member.send(`Please move the role ** Noro Lex Studio Bot ** in first place in the order of the roles top, create a ** Music ** role for use a music commands and create a ** Muted ** role fpr mute user`); 
 				 
-				
+				if(message.guild.channels.cache.find(channel => channel.name === `bot-log`)) {
+            return message.reply('you already have setup the server');
+        }
+				 message.guild.channels.create(`Bot-Log`, {
+            permissionOverwrites: [
+                {
+                    id: message.guild.roles.everyone,
+                    deny: ['VIEW_CHANNEL'],
+                },
+            ],
+            type: 'text',
+        }).then(async channel => {
+            message.reply('you have successfully setup the server')
 				 
-				 
-    }
-};
+    })
+}};
+
