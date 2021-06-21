15ea5bde4237bf894d74e606550aaa5ef2ed6c19
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-06-21 21:56:46 +0200

    Update close.js

--- commands/moderation/close.js
+++ commands/moderation/close.js
@@ -1,21 +1,41 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: 'close',
     aliases: ['del'],
     category: 'Moderation',
     utilisation: '{prefix}close',
 	execute(client, message, args) {
+        
+        if(!message.guild.channels.cache.find(channel => channel.name === `bot-log`)) {
+            return message.reply('you have not setup the server');
+        }
+        
 		           if (!message.member.hasPermission("ADMINISTRATOR")) {
                     return message.reply(
                         "You do not have the permission to use that command."
                     );
                 }
+        
+        let moderationEmbed = new MessageEmbed()
+            .setColor('#ff0000')
+            .setTitle("**__Moderation System__**")
+            .setDescription(`**<@${message.author.id}> close a ticket**`)
+            .addField(`**Action:**`, `\`ticket\``)
+            .addField(`**Ticket:**`, `#ticket-error`)
+        
 		
 		if(message.channel.name.includes('ticket-')) {
 			message.channel.delete();
-		}
+            
+            let logchannel = message.guild.channels.cache.find(channel => channel.name === `bot-log`)
+			if(logchannel) {
+				logchannel.send(moderationEmbed);
+                
+		}}
 		else {
 			return message.reply('you cannot use this command here. Please use this command when you want to delete a ticket.');
 		}
-		
+		 
 	},
 };
