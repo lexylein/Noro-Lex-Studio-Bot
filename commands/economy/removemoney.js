468473dc30acc0451f1290acfc34b16dabfce83f
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-07-06 11:28:19 +0200

    Update removemoney.js

--- commands/economy/removemoney.js
+++ commands/economy/removemoney.js
@@ -18,8 +18,8 @@ execute(client, message, args) {
     if (!user) return message.channel.send("**Enter A Valid User!**");
 
     if (isNaN(args[1])) return;
-    db.subtract(`money_${user.id}`, args[1])
-    let bal = db.fetch(`money_${user.id}`)
+    db.subtract(`money_${message.guild.id}_${user.id}`, args[1])
+    let bal = db.fetch(`money_${message.guild.id}_${user.id}`)
 
   message.channel.send(`Taken \`${args[1]}\` credits from **${user}**'s balance.\n> Current balance: \`${bal}\` credits.`)
 
