1ce2b0096832bac5700a9b76bb3ca217c700570f
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-07-06 10:54:52 +0200

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -4,8 +4,8 @@ const db = require("quick.db");
 
 module.exports = {
     name: 'daily',
-    aliases: ['h'],
-    category: 'Prime',
+    aliases: [],
+    category: 'Economy',
     utilisation: '{prefix}daily',
     execute(bot, message, args) {
     
@@ -14,7 +14,7 @@ module.exports = {
         let timeout = 86400000;
         let amount = 200;
 
-        let daily = db.fetch(`daily_${user.id}`);
+        let daily = db.fetch(`daily_${message.guild.id}_${user.id}`);
 
         if (daily !== null && timeout - (Date.now() - daily) > 0) {
             
@@ -28,8 +28,8 @@ module.exports = {
                 .setColor("#ff0000")
                 .setDescription(`✅ You've collected your daily reward of ${amount} coins`);
             message.channel.send(moneyEmbed)
-            db.add(`money_${user.id}`, amount)
-            db.set(`daily_${user.id}`, Date.now())
+            db.add(`money_${message.guild.id}_${user.id}`, amount)
+            db.set(`daily_${message.guild.id}_${user.id}`, Date.now())
 
 
         }
