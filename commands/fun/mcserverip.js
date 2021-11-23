fd624af048f4583a848398ed6b203d89db46033c
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-11-23 15:52:50 +0100

    Delete commands directory

--- commands/fun/mcserverip.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'minecraftserverip',
-    aliases: ['mcserver', 'mcip', 'minecraftserver', 'minecraftip', 'mcserverip'],
-    category: 'Fun',
-    utilisation: '{prefix}minecraftserverip',
-execute(client, message, args) {  
-    message.author.send('the server IP from Noro_Lex`s minecraft server is: schafscraft.de')
-	}
-};
