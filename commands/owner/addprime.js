3243616465167e981e00dc691c9963ba5644f756
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2021-12-04 17:14:55 +0100

    Add files via upload

---
+++ commands/owner/addprime.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+    name: 'addprime',
+    aliases: ['addp'],
+    category: 'Owner',
+    utilisation: '{prefix}addprime',
+execute(client, message, args) {
+    
+    if(!message.author.id === "612589827262644234") return message.channel.send('you are not a Owner from the Bot');
+
+    db.set(`prime_${message.guild.id}`, 1)
+    message.channel.send(`Server wurde als Prime Server Hinzugefügt`)
+
+}};
---
+++ commands/owner/removeprime.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+    name: 'removeprime',
+    aliases: ['removep'],
+    category: 'Owner',
+    utilisation: '{prefix}removeprime',
+execute(client, message, args) {
+    
+    if(!message.author.id === "612589827262644234") return message.channel.send('you are not a Owner from the Bot');
+
+    db.set(`prime_${message.guild.id}`, 0)
+    message.channel.send(`Server wurde als Prime Server Entfernt`)
+
+}};
